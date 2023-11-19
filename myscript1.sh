f936fefa9ae721020d55d8b95c2d6233e2a80140
taimumarfat10 <taimumarfat@gmail.com>
2023-11-19 12:59:40 +0600

    basic bash practive

---
+++ myscript1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "This is first script"
+echo "This is my working directory"
+pwd
---
+++ variable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+myname='Taimum'
+myage='32'
+
+echo "Hello, my name is $myname."
+echo "I am $myage years old."
+
+now=$(date)
+
+echo "Todays date and Time is:"
+echo $now
+
+echo "Your PC user name is: $USER"
