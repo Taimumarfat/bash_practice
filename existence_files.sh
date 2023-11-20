f53fb0dbd6c98cc0a5a5161aee2c7082b0e3f0cf
taimumarfat10 <taimumarfat@gmail.com>
2023-11-20 15:36:53 +0600

    updated

---
+++ existence_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -f ~/myfile ]
+then
+    echo "THe file exists."
+else
+    echo "The file not exists."
+fi
---
+++ if_statement.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mynum=400
+
+if [ $mynum -gt 200 ]
+then
+    echo "The Condition is true."
+else
+    echo "The condition is wrong."
+fi
